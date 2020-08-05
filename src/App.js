9960029ef8b6f9329a8827fc4d7e146416d51fc2
shivaji <shivajithattikota@outlook.com>
2020-08-05 15:28:33 +0530

    Understanding button state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: "Max", age:28 },
+      { name: "Manu", age:29 },
+      { name: "Stephaine", age:26 },
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // Don't Do this this.state.persons[0].name = "Maxi";
+    this.setState({
+      persons: [
+        { name: "Maximilian", age:28 },
+        { name: "Manu", age:29 },
+        { name: "Stephaine", age:27 },
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="26">My hobbies are racing </Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
 
